107dc222364478e6dcc46211728d9497fc73c352
objectorange <objectorange@protonmail.com>
2019-05-30 10:02:18 -0400

    Add a packet for requesting content via URL.

---
+++ src/main/java/io/onemfive/proxy/packet/RequestContentPacket.java
@@ -0,0 +1,52 @@
+package io.onemfive.proxy.packet;
+
+import io.onemfive.data.content.Content;
+
+import java.net.URL;
+
+/**
+ * {@link ProxyPacket} for requesting {@link Content}
+ * from a {@link java.net.URL} using
+ * a {@link io.onemfive.proxy.ProxyClient}
+ *
+ * @author objectorange
+ */
+public class RequestContentPacket extends ProxyPacket {
+
+    private URL url;
+    private Content content;
+
+    public RequestContentPacket() {
+        super(false);
+    }
+
+    public RequestContentPacket(boolean hashCashIt) {
+        super(hashCashIt);
+    }
+
+    public RequestContentPacket(URL url) {
+        super(false);
+        this.url = url;
+    }
+
+    public RequestContentPacket(URL url, boolean hashCashIt) {
+        super(hashCashIt);
+        this.url = url;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
+
+    public void setUrl(URL url) {
+        this.url = url;
+    }
+
+    public Content getContent() {
+        return content;
+    }
+
+    public void setContent(Content content) {
+        this.content = content;
+    }
+}
