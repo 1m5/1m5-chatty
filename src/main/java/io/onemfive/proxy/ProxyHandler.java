1de5847c0b39948a9cf146f47f7da4b16d10358d
objectorange <objectorange@1m5.io>
2019-08-30 15:13:32 -0400

    override handle

--- src/main/java/io/onemfive/proxy/ProxyHandler.java
+++ src/main/java/io/onemfive/proxy/ProxyHandler.java
@@ -2,6 +2,14 @@ package io.onemfive.proxy;
 
 import io.onemfive.clearnet.server.EnvelopeProxyDataHandler;
 import io.onemfive.data.Envelope;
+import io.onemfive.data.util.DLC;
+import io.onemfive.sensors.SensorsService;
+import org.eclipse.jetty.server.Request;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.logging.Logger;
 
 /**
@@ -20,6 +28,23 @@ public class ProxyHandler extends EnvelopeProxyDataHandler {
 
     public static void setProxyService(ProxyService client) {
         proxyService = client;
+    }
+
+    @Override
+    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+        Envelope envelope = this.parseEnvelope(request);
+        EnvelopeProxyDataHandler.ClientHold clientHold = new EnvelopeProxyDataHandler.ClientHold(target, baseRequest, request, response, envelope);
+        requests.put(envelope.getId(), clientHold);
+        DLC.addRoute(SensorsService.class, "SEND", envelope);
+        route(envelope);
+        if (DLC.getErrorMessages(envelope).size() > 0) {
+            LOG.warning("Returning HTTP 500...");
+            response.setStatus(500);
+            baseRequest.setHandled(true);
+            requests.remove(envelope.getId());
+        } else {
+            clientHold.hold(600000L);
+        }
     }
 
     /**
